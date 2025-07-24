c4c79a4b3596de4cae6aa4d7e41e587fe18a77de
Andra Antariksa <andra@stamps.id>
2025-07-24 23:33:58 +0700

    feat: add django admin

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Book, BookStock
+
+
+class BookStockInline(admin.TabularInline):
+    model = BookStock
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "isbn")
+    search_fields = ("title", "isbn")
+    inlines = [BookStockInline]
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active")
