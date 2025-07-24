b28930c0ba4a5c49630d82fca04edbf82cc243e3
Andra Antariksa <andra@stamps.id>
2025-07-24 23:10:49 +0700

    feat: update book borrow transaction model (#12)

--- apps/borrows/models.py
+++ apps/borrows/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+from apps.common.models import TimestampedModel
 
-class BookBorrowTransaction(models.Model):
+
+class BookBorrowTransaction(TimestampedModel):
     book = models.ForeignKey(
         "books.Book", on_delete=models.CASCADE, related_name="borrow_transactions"
     )
     borrower = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="borrow_transactions"
     )
-    return_date = models.DateField(db_index=True)
+    return_scheduled_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
 
     class Meta:
         verbose_name = "book_borrow_transactions"
