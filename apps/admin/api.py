a439903f7b4f385d910017fdaefd8eeea2275b8d
Andra Antariksa <andra@stamps.id>
2025-07-24 19:29:06 +0700

    feat: add create book feature

--- apps/admin/api.py
+++ apps/admin/api.py
@@ -1,8 +1,10 @@
 from ninja import Router
 from django.contrib.auth import authenticate, login
 from django.http import HttpRequest
+from ninja.security import django_auth
 
-from apps.admin.schema import AdminLoginIn
+from apps.books.models import Book
+from apps.admin.schema import AdminLoginIn, AdminCreateBook
 
 
 router = Router()
@@ -16,3 +18,12 @@ def admin_login(request: HttpRequest, data: AdminLoginIn):
         return 200, None
 
     return 400, None
+
+
+@router.post("/admin/books", response={201: None}, auth=django_auth)
+def books_create(request: HttpRequest, data: AdminCreateBook):
+    # Convert schema to dict and create Book instance
+    book_data = data.dict()
+    book = Book(**book_data)
+    book.save()
+    return 201, None
--- apps/admin/schema.py
+++ apps/admin/schema.py
@@ -1,6 +1,14 @@
-from ninja import Schema
+from ninja import Schema, ModelSchema
+
+from apps.books.models import Book
 
 
 class AdminLoginIn(Schema):
     email: str
     password: str
+
+
+class AdminCreateBook(ModelSchema):
+    class Meta:
+        model = Book
+        fields = ["title", "isbn"]
