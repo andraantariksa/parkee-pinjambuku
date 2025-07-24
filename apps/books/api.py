d0d0e37f8e5c5aa07d33dbd114f7fd251a71aaf6
Andra Antariksa <andra@stamps.id>
2025-07-24 18:25:53 +0700

    List books (#5)
    * feat: add books migration
    
    * feat: add list books api
    
    * feat: update url
    
    * style: simplify

---
+++ apps/books/api.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest
+from ninja import Router
+
+from apps.books.models import Book
+from apps.books.schemas import BookSchema
+
+
+router = Router()
+
+
+@router.get("books/", response={200: list[BookSchema]})
+def list_books(request: HttpRequest):
+    return Book.objects.all()
--- apps/books/tests.py
+++ apps/books/tests.py
@@ -1,1 +1,25 @@
-# Create your tests here.
+from json import loads
+from django.test import TestCase
+
+from ninja.testing import TestClient
+
+from apps.books.models import Book
+from apps.books.api import router
+
+
+class BooksTestCase(TestCase):
+    def setUp(self):
+        self.client = TestClient(router)
+
+    def test_list_books(self):
+        Book.objects.create(
+            title="Book 1",
+            isbn="111",
+        )
+
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            loads(response.content),
+            [{"id": 1, "isbn": "1", "title": "Book 1"}],
+        )
