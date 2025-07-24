ff86e80e334c39341d6333219c05bfe6cde0e987
Andra Antariksa <andra@stamps.id>
2025-07-24 19:29:10 +0700

    test: add test

--- apps/admin/tests.py
+++ apps/admin/tests.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.test import TestCase
 
 
+from apps.books.models import Book
 from apps.admin.api import router
 from apps.users.models import User
 from tests.utils import SessionTestClient
@@ -10,28 +12,44 @@ class AdminTest(TestCase):
     def setUp(self) -> None:
         self.email = "admin@example.com"
         self.password = "123"
-        self.admin_user = User.objects.create(name="Andra", email=self.email)
+        self.admin_user = User.objects.create(
+            name="Andra", email=self.email, is_staff=True
+        )
         self.admin_user.set_password(self.password)
         self.admin_user.save()
         self.client = SessionTestClient(router)
 
-    def test_admin_login(self) -> None:
+    def login(
+        self, email: str | None = None, password: str | None = None
+    ) -> HttpResponse:
         login_data = {
-            "email": "wrong@email.com",
-            "password": "wrongpassword",
+            "email": email or self.email,
+            "password": password or self.password,
         }
-        response = self.client.post("/admin/login", json=login_data)
+        return self.client.post("/admin/login", json=login_data)
+
+    def test_admin_login(self) -> None:
+        response = self.login(email="wrong@email.com", password="wrongpassword")
         self.assertEqual(response.status_code, 400)
 
-        login_data = {
-            "email": self.email,
-            "password": self.password,
-        }
-        response = self.client.post("/admin/login", json=login_data)
+        self.admin_user.is_staff = False
+        self.admin_user.save(update_fields=["is_staff"])
+
+        response = self.login()
         self.assertEqual(response.status_code, 400)
 
         self.admin_user.is_staff = True
         self.admin_user.save(update_fields=["is_staff"])
 
-        response = self.client.post("/admin/login", json=login_data)
+        response = self.login()
         self.assertEqual(response.status_code, 200)
+
+    def test_books_create(self) -> None:
+        response = self.login()
+
+        book_data = {"title": "Test Book", "isbn": "1234567890"}
+        response = self.client.post("/admin/books", json=book_data)
+        self.assertEqual(response.status_code, 201)
+
+        book_exists = Book.objects.filter(title="Test Book", isbn="1234567890").exists()
+        self.assertTrue(book_exists)
