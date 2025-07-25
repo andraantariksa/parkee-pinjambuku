2c4a4eb67c80f7a0bae31a0e39be76ea1eecf93f
Andra Antariksa <andra@stamps.id>
2025-07-25 09:24:40 +0700

    feat: init react (#16)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BookList from './components/BookList';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<BookList />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <>
+      <p>Hi there</p>
+    </>
   );
 }
