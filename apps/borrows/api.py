79b5b36d5a8d845f9855e0c31f0ecb62047ec191
Andra Antariksa <andra@stamps.id>
2025-07-25 14:36:19 +0700

    fix: update error response

--- apps/borrows/api.py
+++ apps/borrows/api.py
@@ -24,7 +24,6 @@ router = Router()
 @router.get(
     "borrowers/{id_card_number}",
     response={200: BorrowerDetailsSchema},
-    auth=django_auth,
 )
 def borrowers_details(request: HttpRequest, id_card_number: str):
     user = get_object_or_404(User, id_card_number=id_card_number)
@@ -35,25 +34,28 @@ def borrowers_details(request: HttpRequest, id_card_number: str):
 
 @router.post(
     "borrowers/{id_card_number}/borrow",
-    response={200: str, 400: str},
+    response={200: None, 400: dict},
 )
 def borrow_book(request: HttpRequest, id_card_number: str, data: BorrowRequestSchema):
-    user = get_object_or_404(User, id_card_number=id_card_number)
+    user = get_object_or_404(User, id_card_number=id_card_number, email=data.email)
     book = get_object_or_404(Book, id=data.book_id)
 
     book_stock, _ = BookStock.objects.get_or_create(book=book)
     if book_stock.quantity < 1:
-        return 400, "Book is out of stock"
+        return 400, {"detail": "Book is out of stock" }
 
     has_active_loan = BookBorrowTransaction.objects.filter(
         borrower=user, return_date__isnull=True
     ).exists()
     if has_active_loan:
-        return 400, "You have an active loan and cannot borrow another book"
+        return 400, { "detail": "You have an active loan and cannot borrow another book" }
+
+    if data.return_scheduled_date < timezone.localdate():
+        return 400, { "detail": "You cant set return date in the past" }
 
     MAX_LOAN_DURATION = timedelta(days=30)
     if data.return_scheduled_date > timezone.localdate() + MAX_LOAN_DURATION:
-        return 400, "Loan duration cannot be longer than 30 days"
+        return 400, { "detail": "Loan duration cannot be longer than 30 days" }
 
     BookBorrowTransaction.objects.create(
         book=book,
@@ -64,12 +66,12 @@ def borrow_book(request: HttpRequest, id_card_number: str, data: BorrowRequestSc
     book_stock.quantity = F("quantity") - 1
     book_stock.save()
 
-    return 200, "Book borrowed successfully"
+    return 200, None
 
 
 @router.post(
     "borrowers/{id_card_number}/return",
-    response={200: None, 400: str},
+    response={200: None, 400: dict},
 )
 def return_book(request: HttpRequest, id_card_number: str, data: ReturnRequestSchema):
     user = get_object_or_404(User, id_card_number=id_card_number, email=data.email)
@@ -81,7 +83,7 @@ def return_book(request: HttpRequest, id_card_number: str, data: ReturnRequestSc
         return_date__isnull=True,
     ).first()
     if not borrow_transaction:
-        return 400, "No active borrow transaction found for this book and user"
+        return 400, {"detail": "No active borrow transaction found for this book and user"}
 
     borrow_transaction.return_date = timezone.localdate()
     borrow_transaction.save()
