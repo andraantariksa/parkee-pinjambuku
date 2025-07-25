972a7d3a4d472602a32aeb1357713c46141a793a
Andra Antariksa <andra@stamps.id>
2025-07-25 15:57:04 +0700

    Book transactions api (#29)
    * feat: book transactions api
    
    * test: add test

--- apps/administrator/api.py
+++ apps/administrator/api.py
@@ -1,10 +1,17 @@
 from ninja import Router
+from django.db.models import Q
+from django.utils import timezone
 from django.contrib.auth import authenticate, login
 from django.http import HttpRequest
 from ninja.security import django_auth
 
 from apps.books.models import Book, BookStock
-from apps.administrator.schema import AdminLoginIn, AdminCreateBook
+from apps.administrator.schema import (
+    AdminLoginIn,
+    AdminCreateBook,
+    BookTransactionsSchema,
+)
+from apps.borrows.models import BookBorrowTransaction
 
 
 router = Router()
@@ -28,3 +35,27 @@ def books_create(request: HttpRequest, data: AdminCreateBook):
     book = Book.objects.create(title=data.title, isbn=data.isbn)
     BookStock.objects.create(book=book, quantity=data.stock_quantity)
     return 201, None
+
+
+@router.get(
+    "/admin/transactions",
+    response={200: BookTransactionsSchema, 400: None},
+    auth=django_auth,
+)
+def books_transactions(request: HttpRequest):
+    if not request.user.is_staff:
+        return 400, None
+
+    from django.db.models import F
+
+    today = timezone.localdate()
+    transactions = BookBorrowTransaction.objects.annotate(
+        is_late=(
+            (Q(return_date__isnull=True) & Q(return_scheduled_date__lt=today))
+            | (
+                Q(return_date__isnull=False)
+                & Q(return_date__gt=F("return_scheduled_date"))
+            )
+        )
+    ).order_by("-id")
+    return 200, {"transactions": transactions}
