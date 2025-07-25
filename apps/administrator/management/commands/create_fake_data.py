f1286b05a229dfeb9575c49dbeca7b094a66c71f
Andra Antariksa <andra@stamps.id>
2025-07-25 09:41:58 +0700

    feat: fake data command (#17)

---
+++ apps/administrator/management/commands/create_fake_data.py
@@ -0,0 +1,38 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from faker import Faker
+from apps.books.models import Book
+from apps.users.models import User
+
+
+class Command(BaseCommand):
+    help = "Create fake data for books and users"
+
+    def handle(self, *args: Any, **kwargs: Any):
+        faker = Faker()
+
+        # Create fake users
+        if not User.objects.filter(email="admin@example.com").exists():
+            user = User(name="Test", email="admin@example.com")
+            user.set_password("123")
+            user.is_staff = True
+            user.is_superuser = True
+            user.is_admin = True
+            user.save()
+
+        for _ in range(5):
+            email = faker.email()
+            if not User.objects.filter(email=email).exists():
+                user = User(name=faker.name(), email=email)
+                user.set_password("123")
+                user.save()
+                self.stdout.write(self.style.SUCCESS(f"Created user: {email}"))
+
+        # Create fake books
+        for _ in range(10):
+            title = faker.sentence(nb_words=4)
+            isbn = faker.isbn13()
+            Book.objects.create(title=title, isbn=isbn)
+            self.stdout.write(self.style.SUCCESS(f"Created book: {title}"))
+
+        self.stdout.write(self.style.SUCCESS("Fake data creation complete."))
