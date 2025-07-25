6fa056ee02213c2f97c4aeb1621a7d2a101f35fd
Andra Antariksa <andra@stamps.id>
2025-07-25 09:41:28 +0700

    feat: basic book list ui (#18)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './components/BookList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <>
-      <p>Hi there</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BookList />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
