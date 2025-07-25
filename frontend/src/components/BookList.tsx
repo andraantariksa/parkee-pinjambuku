26ee76525d25a6cd06c4c4bc7d2aa740af7aa02a
Andra Antariksa <andra@stamps.id>
2025-07-25 12:09:35 +0700

    Prettier (#19)
    * chore: add prettier
    
    * style: prettier

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 interface Book {
   id: number;
@@ -8,40 +8,33 @@ interface Book {
 }
 
 const fetchBooks = async (): Promise<Book[]> => {
-  const response = await fetch('http://127.0.0.1:8000/api/v1/books/');
+  const response = await fetch("http://127.0.0.1:8000/api/v1/books");
   if (!response.ok) {
-    throw new Error(`Error fetching books [${response.status}]`);
+    throw new Error(`${response.status} status code`);
   }
   return response.json();
 };
 
 const BookList: React.FC = () => {
   const { data, error, isLoading } = useQuery<Array<Book>, Error>({
-    queryKey: ['books'],
+    queryKey: ["fetch-books"],
     queryFn: fetchBooks,
   });
-
-  let content;
-  if (isLoading) {
-    content = <div>Loading...</div>;
-  } else if (error) {
-    content = <div>Error: {error.message}</div>;
-  } else {
-    content = (
-      <ul>
-        {data?.map((book: Book) => (
-          <li key={book.id}>
-            <strong>{book.title}</strong> (ISBN: {book.isbn})
-          </li>
-        ))}
-      </ul>
-    );
-  }
 
   return (
     <div>
       <h1>Book List</h1>
-      {content}
+      {isLoading && <div>Loading...</div>}
+      {error && <div style={{ color: "red" }}>Error: {error.message}</div>}
+      {data && (
+        <ul>
+          {data.map((book: Book) => (
+            <li key={book.id}>
+              <strong>{book.title}</strong> (ISBN: {book.isbn})
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
